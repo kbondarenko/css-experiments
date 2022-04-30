e9f813e3047f538125c864713ea61c0ee78867ba
Kirill Bondarenko <kirill.bondarenko.1987@gmail.com>
2022-04-30 22:48:18 +0500

    used svelteKit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<h1>Markup experiments</h1>
+
+<nav>
+  <ul>
+    <li>
+      <h2>Paper effect</h2>
+      <ul>
+        <li><a href="/paper-effect/sheets-of-paper">Sheets of paper</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
