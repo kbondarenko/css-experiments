3b4813fa4fa3a216af9aea588b41c497c9d7c4ec
Kirill Bondarenko <kirill.bondarenko.1987@gmail.com>
2023-06-21 22:47:27 +0500

    replace with vite + react

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <>
+      <div>
+          Test
+      </div>
+    </>
+  )
+}
+
+export default App
